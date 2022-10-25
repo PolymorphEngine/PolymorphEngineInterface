c200a6850e15d5e5faa6e077920e72b6a5085f3c
Tristan GREMONT <tristan.gremont@epitech.eu>
2022-10-25 10:07:08 +0200

    feat: ignored logs

---
+++ Game/Factory/include/ScriptFactory.hpp
@@ -0,0 +1,57 @@
+/*
+** EPITECH PROJECT, 2022
+** ScriptFactory.hpp
+** File description:
+** header for ScriptFactory.cpp
+*/
+
+#pragma once
+
+#include "Polymorph/Core.hpp"
+
+//@Initalizers
+//${INITIALIZERS_INCLUDES}
+
+//@Scripts
+//${SCRIPTS_INCLUDES}
+
+
+namespace Polymorph
+{
+    class ScriptFactory : public IScriptFactory
+    {
+
+////////////////////// CONSTRUCTORS/DESTRUCTORS /////////////////////////
+        public:
+            ~ScriptFactory() override = default;
+
+
+//////////////////////--------------------------/////////////////////////
+
+//Builder template : {"${SCRIPT_NAME}", [](Config::XmlComponent &data, GameObject entity) -> Initializer{ return Initializer(new ${SCRIPT_NAME}Initializer(data, entity));}},
+
+///////////////////////////// PROPERTIES ////////////////////////////////
+        private:
+            using FactoryLambda = std::function<Initializer (Config::XmlComponent &data, GameObject entity)>;
+            using F = auto (Config::XmlComponent &data, GameObject entity) -> Initializer;
+            static const inline std::map<std::string, FactoryLambda>
+                _buildables =
+                {
+                    //${BUILDERS}
+                };
+//////////////////////--------------------------/////////////////////////
+
+
+
+/////////////////////////////// METHODS /////////////////////////////////
+        public:
+            Initializer create(std::string &type, Config::XmlComponent &data,
+            GameObject entity) override;
+            bool hasType(std::string &type) final;
+//////////////////////--------------------------/////////////////////////
+
+    };
+}
+
+
+
---
+++ Game/Factory/src/ScriptFactory.cpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2022
+** ScriptFactory.cpp
+** File description:
+** ScriptFactory.cpp
+*/
+#include "ScriptFactory.hpp"
+
+Polymorph::Initializer Polymorph::ScriptFactory::create(std::string &type, Config::XmlComponent &data,
+                                 GameObject entity)
+{
+    return _buildables.at(type)(data, entity);
+}
+
+bool Polymorph::ScriptFactory::hasType(std::string &type)
+{
+    return _buildables.contains(type);
+}
