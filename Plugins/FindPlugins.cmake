c22e657d33d4eb3b5c077709f92fe0290c69d46d
Tristan Gremont <tristan.gremont@epitech.eu>
2022-10-25 22:38:44 +0200

    feat: added cmake fetch plugin management

---
+++ Plugins/FindPlugins.cmake
@@ -0,0 +1,47 @@
+if(NOT TARGET render-2D)
+    include(FetchContent)
+
+    FetchContent_Declare(
+            render-2D
+            GIT_REPOSITORY git@github.com:PolymorphEngine/render-2D.git
+            GIT_TAG main
+    )
+
+    FetchContent_MakeAvailable(render-2D)
+endif()
+
+if(NOT TARGET render-core)
+    include(FetchContent)
+
+    FetchContent_Declare(
+            render-core
+            GIT_REPOSITORY git@github.com:PolymorphEngine/render-core.git
+            GIT_TAG main
+    )
+
+    FetchContent_MakeAvailable(render-core)
+endif()
+
+if(NOT TARGET raylib-render-core)
+    include(FetchContent)
+
+    FetchContent_Declare(
+            raylib-render-core
+            GIT_REPOSITORY git@github.com:PolymorphEngine/raylib-render-core.git
+            GIT_TAG main
+    )
+
+    FetchContent_MakeAvailable(raylib-render-core)
+endif()
+
+if(NOT TARGET raylib-render-2D)
+    include(FetchContent)
+
+    FetchContent_Declare(
+            raylib-render-2D
+            GIT_REPOSITORY git@github.com:PolymorphEngine/raylib-render-2D.git
+            GIT_TAG main
+    )
+
+    FetchContent_MakeAvailable(raylib-render-2D)
+endif()
